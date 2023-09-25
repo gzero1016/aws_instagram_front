9d54b9dc9d4f60deecd6057ae704d3951727d644
gzero1016 <gzero1016@gmail.com>
2023-09-25 16:40:23 +0900

    feat: AuthRoute 조건에맞는 페이지접근권한

--- front/src/auth/AuthRoute.jsx
+++ front/src/auth/AuthRoute.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Navigate, useLocation, useNavigate } from 'react-router-dom';
 
-function AuthRoute(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+function AuthRoute({ element }) {
+    const location = useLocation();
+    const pathname = location.pathname;
+    const permitAllPath = ["/accounts"];
+    const [ authenticated, setAuthenticated ] = useState(false);
+
+    for(let path of permitAllPath) {  // for문으로 들어오는 path들의
+        if(pathname.startsWith(path)) { // 경로가 /accounts 로 시작하는지?
+            if(authenticated) { // 인증된 사람인지?
+                return <Navigate to={"/"}/>;
+            }
+            return element;
+        }
+    }
+
+    if(!authenticated) {
+        return <Navigate to={"/accounts/login"}/>;
+    }
+    return element;
 }
 
 export default AuthRoute;
